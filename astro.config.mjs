3fbaba56c87f8f4b1d10bdb0093ab8f4c2c4381b
sandbox co.,ltd <info@sndbox.jp>
2026-06-02 12:04:19 +0900

    e

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 import sitemap from '@astrojs/sitemap'
-// import rehypeFigure from 'rehype-figure'
 import rehypeFigure from '@microflash/rehype-figure' // こっちに変える
 import starlightImageZoom from 'starlight-image-zoom'
 import starlightSidebarTopics from 'starlight-sidebar-topics'
